58926601d1160e073551e6bae20b757567f4fc76
kislenok-roman <kislenok.roman@gmail.com>
2016-04-09 12:24:12 +1000

    * done assignment1

--- capstone/blight/assignment1.R
+++ capstone/blight/assignment1.R
@@ -4,11 +4,13 @@
 require(ggplot2)
 require(ggmap)
 
+## DRAW A MAP
 q1 <- ggplot(data = all[type != "detroit-demolition-permits"], aes(x = lon, y = lat))
+# can't use google maps here, I've spent 2500 request while testing...
 map <- get_map(location = c(all[, min(lon)], all[, min(lat)], all[, max(lon)], all[, max(lat)]),
                zoom = "auto", scale = 575000, source = "osm")
 q1 <- ggmap(map, base_layer = q1, darken = c(0.2, "white"))
-q2 <- q1 +
+q1 <- q1 +
     labs(color = "Density") +
     geom_point(alpha = 0.05, fill = "black", color = "black") +
     ggtitle("Detroit incidents density") +
@@ -16,4 +18,61 @@ q2 <- q1 +
                color = "red",
                size = 1)
 
-q2
+q1
+
+## EVENTS ARE SPREAD ACROSS TIME!!
+ggplot(merge(x = all[, list(dt = seq(min(dt), max(dt), by = "1 day")), list(type)],
+             y = all[, list(cnt = .N), list(dt, type)],
+             by = c("dt", "type"),
+             all.x = TRUE)[is.na(cnt), cnt := 0][, list(cnt = sum(cnt), dt = median(dt)), by = list(type, mon = substr(dt, 1, 7))],
+       aes(x = dt, y = cnt, color = type)) +
+    geom_point(size = 3) +
+    geom_line(size = 2) +
+    theme_bw() +
+    ylab("") +
+    xlab("") +
+    scale_color_hue("") +
+    ggtitle("Event in time")
+
+
+# 2. In your set of derived buildings, are you noticing any outliers spatially?
+# Consider what might be causing these outliers, and how you might specially handle them.
+# In data.R I've tried to anser this question with code))
+
+# 3. How did you use the geocoordinates to define a "building"?
+# How might you need to change your approach if you were working with 100x the number of incidents?
+# How might your solution change if conditions of the problem change?
+# Would high-density areas like Manhattan in New York City make differentiation between buildings more difficult?
+# Are there heuristics used about how buildings tend to be arranged geometrically that may be violated in certain places?
+# None of these issues are necessarily a problem, but the more general the solution, the more likely it is to be used in additional cities.
+## I try to write down a function to search dataset for events
+
+# utility function to calculate date in past from `dt`
+diff <- function(dt, period) {
+    seq(as.Date(dt), by = paste0("-", period), length.out = 2)[2]
+}
+#
+# all - our dataset, should have lat & lon columns
+# max.dist - max distance in meters from (lon0, lat0) point, optimaly we will need a decay function, but we can apply it further
+# max.timespan - max timespan from dt0, optimally we will need a decay function, but
+# lon0, lat0 - base point spacial location
+# dt0 - base point time position
+eventsForPoint <- function(all, lat0, lon0, dt0, max.dist = 500, max.timespan = "1 year") {
+    # degree -> radian
+    rad <- function(d) {
+        d * pi / 180
+    }
+    R <- 6371000 # Earth radius
+    # filter by date & we need type,
+    all[dt >= diff(dt0, max.timespan) & dt < dt0,
+                   list(type,
+                        code,
+                        timespan = dt0 - dt,
+                        distance = acos(sin(rad(lat0)) * sin(rad(lat)) + cos(rad(lat0)) * cos(rad(lat)) * cos(rad(lon - lon0))) * R)]
+}
+decayDistance <- function(meters) {
+    1 / meters ^ 2
+}
+decayTimespan <- function(timespan) {
+    1 / timespan
+}
--- capstone/blight/data.R
+++ capstone/blight/data.R
@@ -1,5 +1,6 @@
 require(data.table)
-require(stringr)
+require(ggmap) # geocode with google
+require(XML) # parse State coords
 
 # process datasets
 # 1. detroit-blight-violations
@@ -11,17 +12,18 @@ d1 <- fread("data/detroit-blight-violations.csv",
 d1 <- d1[, list(id = TicketID,
                 type = "detroit-blight-violations",
                 address = toupper(paste0(ViolationStreetNumber, " ", ViolationStreetName)),
-                dt = paste0(substr(TicketIssuedDT, 7, 10), "-", substr(TicketIssuedDT, 1, 2), "-", substr(TicketIssuedDT, 4, 5), " ", str_pad(TicketIssuedTime, width = 8, side = "left", pad = "0")),
+                dt = paste0(gsub("^([0-9]+)/([0-9]+)/([0-9]+).*", "\\3-\\1-\\2", TicketIssuedDT)),
                 code = ViolationCode,
                 lat = as.numeric(gsub(".*\\n[(]([0-9.\\-]+).*", "\\1", ViolationAddress)),
                 lon = as.numeric(gsub(".*, ([0-9.\\-]+)[)]$", "\\1", ViolationAddress)))]
 
-# remove dirty dates
+# remove dirty dates - it seems that we can find some specific rule to process them, but we would not
+# it'll be not so universal & we really need not too old problems (before 2005)
 d1 <- d1[dt < '2100' & dt > '2000']
 d1 <- d1[,
          list(type,
-              address = last(address), # there is duplicates
-              dt = last(dt),
+              address = last(address), # there are duplicates
+              dt = as.Date(last(dt)),
               code = last(code),
               lat = last(lat),
               lon = last(lon)),
@@ -31,20 +33,105 @@ d1 <- d1[,
 # 2. detroit-311
 d2 <- fread("data/detroit-311.csv")
 
-d2 <- d2
-# d2[,  as.Date(paste0(substr(ticket_created_date_time, 7, 10), "-", substr(ticket_created_date_time, 1, 2), "-", substr(ticket_created_date_time, 4, 5)))))]
-# 2014-07-14 -- 2015-10-15
+d2 <- d2[, list(id = ticket_id,
+                type = "detroit-311",
+                address = toupper(gsub("Detroit.*", "", address)),
+                dt = as.Date(gsub("^([0-9]+)/([0-9]+)/([0-9]+).*$", "\\3-\\1-\\2", ticket_created_date_time)),
+                code = issue_type,
+                lat,
+                lon = lng)]
+d2 <- unique(d2)
 
-# Преступления
-d3 <- fread("data/_dcebfb2135a2bf5a6392493bd61aba22_detroit-crime.csv",
+# 3. detroit-crime
+d3 <- fread("data/detroit-crime.csv",
             colClasses = list(character = 3),
             verbose = TRUE)
+# it seems that here we have some events split into multiple row with the same info and
+# diff categories
+d3 <- d3[, list(type = "detroit-crime",
+                address = toupper(ADDRESS[1]),
+                dt = as.Date(gsub("([0-9]+)/([0-9]+)/([0-9]+).*", "\\3-\\1-\\2", INCIDENTDATE[1])),
+                code = paste0(CATEGORY, collapse = ", "),
+                lat = median(LAT),
+                lon = median(LON)),
+         by = list(id = CASEID)]
 
-# Разрешние на снос зданий
-d4 <- fread("data/_dcebfb2135a2bf5a6392493bd61aba22_detroit-demolition-permits.tsv",
+# 4. detroit-demolition-permits
+d4 <- fread("data/detroit-demolition-permits.tsv",
             colClasses = list(character = c(38, 40, 43, 45, 47, 51, 55)))
 
-# ДОБАВОЧКИ
+# there are some warnings about coords
+d4 <- d4[, list(type = "detroit-demolition-permits",
+                address = toupper(SITE_ADDRESS[1]),
+                dt = as.Date(gsub("([0-9]+)/([0-9]+)/([0-9]+)", "20\\3-\\1-\\2", PERMIT_APPLIED[1])),
+                code = "Dismantle",
+                lat = median(as.numeric(gsub(".*[(]([0-9.\\-]+).*", "\\1", site_location))),
+                lon = median(as.numeric(gsub(".*, ([0-9.\\-]+)[)]$", "\\1", site_location)))),
+         by = list(id = PERMIT_NO)]
+
+# unite
+all <- rbind(d1, d2, d3, d4)
+rm(d1, d2, d3, d4)
+
+# THERE ARE SOME MISSING/WRONG COORDINATES - we can try to get data from google
+# but for free we can only get 2500 requests per day...
+# or we can try to figure coords by addresses
+
+# CAUTION! This is city-region-specific. I can't figure out where to get city boundary (or if they even exists)
+# so just drop obviously bad coords for state (MI)
+# coords for states (http://econym.org.uk/gmap/states.xml)
+state_coord_doc <- xmlInternalTreeParse("http://econym.org.uk/gmap/states.xml")
+lats_MI <- as.numeric(xpathSApply(state_coord_doc, "/states/state[@name='Michigan']/point/@lat"))
+lons_MI <- as.numeric(xpathSApply(state_coord_doc, "/states/state[@name='Michigan']/point/@lng"))
+all[lat < min(lats_MI) | lon < min(lons_MI) | lat < min(lats_MI) | lat > max(lats_MI), `:=`(lat = NA, lon = NA)]
+free(state_coord_doc)
+rm(state_coord_doc, lats_MI, lons_MI)
+
+# but look - there is a lot of outliers even after our "not so precious" clean
+boxplot(all[, lat])
+all[lat %in% boxplot(all[, lat], plot = FALSE)$out, `:=`(lat = NA, lon = NA)]
+boxplot(all[, lon])
+all[lon %in% boxplot(all[, lon], plot = FALSE)$out, `:=`(lat = NA, lon = NA)]
+
+# fix addresses
+all[, address := gsub("^[ ]+|[ ]+$", "", address)] # lead & trail spece
+all[, address := gsub("[ ]+", " ", address)] # inner space
+
+# try to obtain coords by addresses
+miss <- all[is.na(lat) | is.na(lon)]
+all <- all[!is.na(lat) & !is.na(lon)]
+miss <- merge(x = miss[, list(id, type, address, dt, code)],
+              y = all[, list(address, lat, lon)],
+              by = "address",
+              all.x = TRUE)[, list(lat = median(lat),
+                                   lon = median(lon)),
+                            by = list(id, type, address, dt, code)]
+
+all <- rbind(all, miss[!is.na(lat) & !is.na(lon)])
+
+miss <- miss[is.na(lat) | is.na(lon)]
+
+## NOTE! It'll take too long time!!!
+if (FALSE) {
+    # HERE WE FIX city, state & country - can't use universaly
+    miss_coord <- geocode(paste0(miss[, address], " , DETROIT, MI, USA"),
+                          source = "google")
+    miss <- cbind(miss[, list(id, type, address, dt, code)], miss_coord)
+    miss[lat < all[, min(lat)] | lat > all[, max(lat)] | lon < all[, min(lon)] | lon > all[, max(lon)], `:=`(lat = NA, lon = NA)]
+
+    all <- rbind(all, miss[!is.na(lat) & !is.na(lon)])
+    rm(miss_coord)
+
+    # 95 incidents still missing...
+}
+rm(miss)
+
+saveRDS(all, "temp/all-data-with-coords-v1.RData")
+
+
+
+## NOTES FOR FUTURE MYSELF
+# Additional datasets to consider
 # 1. Local Employment Dynamics OnTheMap (OTM)
 # 2. American Community Survey
 # 3. The Michigan Liquor Control Commission (MLCC) provided information on the location of
@@ -53,9 +140,9 @@ d4 <- fread("data/_dcebfb2135a2bf5a6392493bd61aba22_detroit-demolition-permits.t
 # information on the location and types of high schools. For this analysis, a school was considered a
 # high school if the grades served included the tenth grade. We measured proximity to a high school
 # by a dummy variable indicating whether the census block was within a half mile of a high school.
+# 5. https://data.detroitmi.gov/browse?category=Property+%26+Parcels&utf8=%E2%9C%93
 
-
-# ПРЕОБРАЗОВАНИЯ
+# TRY TO DO spatial autocorrelation
 # http://www.r-bloggers.com/endogenous-spatial-lags-for-the-linear-regression-model/
 # Criminal offense data recorded by the Detroit Police Department (DPD) were provided to us at
 # point level, with address, latitude and longitude coordinates, offense date, DPD offense category (a
